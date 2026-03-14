aad3015b73cecd1d2b23305dfcdba05f34b95f97
sumitkumar98313 <sumitkumar9867832@gmail.com>
2026-03-14 10:22:00 +0530

    Initial Django project setup

---
+++ myproject/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for myproject project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+
+application = get_wsgi_application()
